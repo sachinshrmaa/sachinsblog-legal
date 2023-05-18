af68f5b1e1b5d7f57e0c1dcd37c1b2e65159c148
Sachin Sharma <sachinkrsharmaa@gmail.com>
2023-05-18 14:18:26 +0530

    added navbar and styles

--- app/layout.js
+++ app/layout.js
@@ -1,6 +1,10 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap-icons/font/bootstrap-icons.css";
 
+import './globals.css'
+
+import Navbar from "@/components/Navbar";
+
 
 export const metadata = {
   title: "SachinsBlog Legal",
@@ -10,7 +14,9 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body>
+        <Navbar />
+        {children}</body>
     </html>
   );
 }
---
+++ components/Navbar.jsx
@@ -0,0 +1,36 @@
+import Link from "next/link";
+
+const Navbar = () => {
+  return (
+    <nav className="navbar navbar-expand-lg navbar-light bg-white">
+      <div className="container">
+        <Link href="/" className="navbar-brand" id="logo">
+          SACHINSBLOG <span className="text-muted ms-2">Legal Help</span>
+        </Link>
+        <button
+          className="navbar-toggler border-0"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarNavAltMarkup"
+          aria-controls="navbarNavAltMarkup"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
+          <div className="navbar-nav ms-auto">
+            <Link href="/support/about" className="nav-link">
+              About
+            </Link>
+            <Link href="/support/contact" className="nav-link ms-md-3">
+              Contact
+            </Link>
+          </div>
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
